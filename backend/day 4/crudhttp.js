a9bd3a66099e67d88ef968a15e6ddfb3ce97e4c5
Vijitverm1234 <143703788+Vijitverm1234@users.noreply.github.com>
2025-02-20 11:15:14 +0530

    added day 4

--- backend/day 4/crudhttp.js
+++ backend/day 4/crudhttp.js
@@ -1,6 +1,24 @@
-const http=require('http')
+const http=require('http');
+const { json } = require('stream/consumers');
+/*
+sever is stateless 
+*/
 const server=http.createServer((req,res)=>{
-    res.write("<h1>Hello Http</h1>")
+    const data=[{
+        id:1,
+        name:'Vijit Verma',
+        email:'ABC@gmail.com'
+    }]
+    const url=req.url;
+    if(url=='/users' && req.method=="GET"){
+        res.writeHead(200,{'content-type':"application/json"});
+        res.write(JSON.stringify(data))
+    }
+    else{
+        res.writeHead(404,{'content-type':"application/json"});
+        res.write(JSON.stringify({msg:"Something went wrong"}))
+    }
+    res.end()
 })
 server.listen(3000,(err)=>{
   try{
